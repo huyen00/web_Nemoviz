fb283ffb2eeaa9e173d978f79ed47c3d572202d3
Hoang Huyen <1851061774@e.tlu.edu.vn>
2022-05-11 18:55:29 +0700

    update

---
+++ js/edit_slick.js
@@ -0,0 +1,223 @@
+$('.slider_cat').slick({
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   arrows: true,
+   autoplay: true,
+   autoplaySpeed: 2500,
+   infinite: true,
+   dots: false,
+   responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 801,
+          settings: {
+            slidesToShow: 3,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 2,
+            arrows: false,
+          }
+        }
+      ]
+ });
+
+$('.slider_business').slick({
+   slidesPerRow: 3,
+   rows: 2,
+   slidesToScroll: 1,
+   arrows: true,
+   autoplay: true,
+   autoplaySpeed: 1500,
+   infinite: true,
+   dots: false,
+   responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 801,
+          settings: {
+            slidesPerRow: 3,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesPerRow: 1,
+            rows: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesPerRow: 1,
+            rows: 2,
+            arrows: false,
+          }
+        }
+      ]
+ });
+
+$('.slider_news').slick({
+   slidesToShow: 2,
+   slidesToScroll: 1,
+   arrows: true,
+   autoplay: true,
+   autoplaySpeed: 1500,
+   infinite: true,
+   dots: false,
+   responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 801,
+          settings: {
+            slidesToShow: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+          }
+        }
+      ]
+ });
+
+
+
+$('.slider_doitac').slick({
+   slidesToShow: 5,
+   slidesToScroll: 1,
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 1500,
+   infinite: true,
+   dots: false,
+   responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 801,
+          settings: {
+            slidesToShow: 4,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 2,
+            arrows: false,
+          }
+        }
+      ]
+ });
+
+
+$(document).ready( function(){
+  $('.slider-gallery').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    asNavFor: '.slider-icon'
+  });
+
+  setTimeout(function () {
+      $('.box_gallery').show();
+  }, 100);
+
+    $('.slider-icon').slick({
+    slidesToShow: 4,
+    asNavFor: '.slider-gallery',
+    arrows: true,
+    focusOnSelect: true, 
+    responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 801,
+            settings: {
+              slidesToShow: 3,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 481,
+            settings: {
+              slidesToShow: 2,
+              arrows: false,
+            }
+          }
+        ]
+  });
+});
+
+$('.slider_brand').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: true,
+   autoplay: true,
+   autoplaySpeed: 1500,
+   infinite: true,
+   dots: false
+});
