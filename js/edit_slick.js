a727b06ffd54a70dcc93b9a56007fca0b17e1e47
Hoang Huyen <1851061774@e.tlu.edu.vn>
2022-05-12 18:18:00 +0700

    update 2

--- js/edit_slick.js
+++ js/edit_slick.js
@@ -118,7 +118,83 @@ $('.slider_news').slick({
       ]
  });
 
+ $('.slider_app ').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  infinite: true,
+  dots: false,
+  responsive: [
+       {
+         breakpoint: 1025,
+         settings: {
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 801,
+         settings: {
+           slidesToShow: 3,
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 481,
+         settings: {
+           slidesToShow: 1,
+           arrows: false,
+         }
+       }
+     ]
+});
 
+$('.slider_product').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  infinite: true,
+  dots: false,
+  responsive: [
+       {
+         breakpoint: 1025,
+         settings: {
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 801,
+         settings: {
+           slidesToShow: 3,
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           arrows: false,
+         }
+       },
+       {
+         breakpoint: 481,
+         settings: {
+           slidesToShow: 1,
+           arrows: false,
+         }
+       }
+     ]
+});
 
 $('.slider_doitac').slick({
    slidesToShow: 5,
